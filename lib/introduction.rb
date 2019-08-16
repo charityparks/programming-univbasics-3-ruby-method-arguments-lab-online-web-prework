644907a4637fe53b571c6a27a6b52478acae21be
Charity Parks <tuppercharity@gmail.com>
2019-08-16 02:37:27 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -8,9 +8,9 @@ end
 def introduction_with_language_optional (name, language)
 puts "Hi, my name is #{name} and I am learning to program in {language}"
 language = Ruby
-end
-def introduction_with_language_optional (name, language = )
-
+end{
+def introduction_with_language_optional (name, language = Ruby)
+ } puts "Hi, my name is #"
 
 
 
